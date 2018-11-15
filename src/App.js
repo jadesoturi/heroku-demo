3db528fb5dfc060f693b5b1e26f2251a8146b2dc
Fjodor Kozlov <fjodork@gmail.com>
2018-11-15 17:48:53 +0100

    Versjon 3

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    Tjohei! Oppdatert versjon 2!
+		    Tjohei! Oppdatert versjon 3!
           </p>
           <a
             className="App-link"
