dae1b938a30df6857f2afb938a6747b54616a5a2
Fjodor Kozlov <fjodork@gmail.com>
2018-11-15 18:19:14 +0100

    Git pull test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-		    Tjohei! Oppdatert versjon 3! Automatic deployment!
+		    Tjohei! Oppdatert versjon 3! Automatic deployment! Pull request test!
           </p>
           <a
             className="App-link"
